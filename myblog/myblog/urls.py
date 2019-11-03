afb5922be51839ccacf7647da36843f5544072d7
Vincent-curly <clvincent@126.com>
2019-11-04 03:33:51 +0800

    针对用户界面的开发--begining

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from .custom_site import custom_site
+from blog.views import post_list, post_detail
+from config.views import links
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', post_list),
+    re_path('category/(?P<category_id>\d+)/', post_list),  # 在Python的正则表达式中，
+    # 分组命名正则表达式组的语法是(?P<name>pattern)，其中name是组的名称，pattern是要匹配的模式
+    re_path('tag/(?P<tag_id>\d+)/', post_list),
+    re_path('post/(?P<post_id>\d+).html', post_detail),
+    re_path('links/', links),
     path('super_admin/', admin.site.urls),
     path('admin/', custom_site.urls),
 ]
